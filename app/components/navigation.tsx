fbce1dea0cfdd7d621a851a7806f3e19843f42b7
nomoto03 <nekomon6030@gmail.com>
2023-08-25 00:32:53 +0900

    loading,error等の各種画面作成

---
+++ app/components/navigation.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import Link from "next/link"
+
+const Navigation = () => {
+  return (
+    <header className="border-b py-5">
+      <div className="container max-w-screen-xl mx-auto relative flex justify-center items-center">
+        <Link href="/" className=" font-bold text-xl cursor-pointer">
+          Next.js supabase Blog
+        </Link>
+
+        <div className="absolute right-5">
+          <div className="flex space-x-4">
+            <Link href="/auth/profile">プロフィール</Link>
+            <Link href="/auth/login">ログイン</Link>
+            <Link href="/auth/signup">サインアップ</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Navigation
